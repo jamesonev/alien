fc530f71528c71920a6efb45190846a725eb9472
Jameson Evans <6jh7ln@gmail.com>
2019-11-24 17:19:09 -0600

    input parsing done

--- atlas/atlas.go
+++ atlas/atlas.go
@@ -1,50 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
-type city struct {
+type City struct {
 	name string
-	n    *city
-	s    *city
-	e    *city
-	w    *city
+	n    *City
+	s    *City
+	e    *City
+	w    *City
 }
 
-func newCity(name string) *city {
-	new := city{name: name}
+func newCity(name string) *City {
+	new := City{name: name}
 	return &new
 }
 
 //these helpers will keep our atlas in sync but will only be available through SetDirection()
-func setNorth(src, dest *city) {
+func setNorth(src, dest *City) {
 	src.n = dest
 	if dest != nil {
 		dest.s = src
 	}
 }
-func setSouth(src, dest *city) {
+func setSouth(src, dest *City) {
 	src.s = dest
 	if dest != nil {
 		dest.n = src
 	}
 }
-func setEast(src, dest *city) {
+func setEast(src, dest *City) {
 	src.e = dest
 	if dest != nil {
 		dest.w = src
 	}
 }
-func setWest(src, dest *city) {
+func setWest(src, dest *City) {
 	src.w = dest
 	if dest != nil {
 		dest.e = src
 	}
 }
 
-func printCity(c *city) {
+func printCity(c *City) {
 	fmt.Print(c.name)
 	if c.n != nil {
 		fmt.Print(" north=", c.n.name)
@@ -60,14 +62,14 @@ func printCity(c *city) {
 	}
 	fmt.Println()
 }
-func printAtlas(atlas map[string]*city) {
+func printAtlas(atlas map[string]*City) {
 	for k := range atlas {
 		printCity(atlas[k])
 	}
 }
 
 // addDirection makes the assumption that if Foo.north is Bar, then Bar.south should be Foo
-func addDirection(atlas map[string]*city, src, dest, direction string) {
+func addDirection(atlas map[string]*City, src, dest, direction string) {
 	sourceCity, exists := atlas[src]
 	if !exists {
 		atlas[src] = newCity(src)
@@ -80,7 +82,8 @@ func addDirection(atlas map[string]*city, src, dest, direction string) {
 	}
 	//make the string lowercase and only compare against the first letter to handle multiple
 	//ways of expressing the same direction: "north", "North", "N", etc
-	direction = strings.ToLower(direction)
+
+	direction = strings.ToLower(strings.TrimSpace(direction))
 	if strings.HasPrefix(direction, "n") {
 		setNorth(sourceCity, destCity)
 	} else if strings.HasPrefix(direction, "s") {
@@ -89,14 +92,51 @@ func addDirection(atlas map[string]*city, src, dest, direction string) {
 		setEast(sourceCity, destCity)
 	} else if strings.HasPrefix(direction, "w") {
 		setWest(sourceCity, destCity)
+	} else {
+		panic(direction)
 	}
 
 }
 
+func parseFile(fileName string) map[string]*City {
+	var atlas = make(map[string]*City)
+	var err error
+	file, err := os.Open(fileName)
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	success := scanner.Scan()
+	for success != false {
+		line := scanner.Text()
+		fields := strings.Fields(line)
+		currentCity := fields[0]
+		directions := fields[1:]
+		for _, direction := range directions {
+			pair := strings.Split(direction, "=")
+			direction = pair[0]
+			city := pair[1]
+			addDirection(atlas, currentCity, city, direction)
+		}
+		success = scanner.Scan()
+	}
+	err = scanner.Err()
+	if err != nil {
+		panic(err)
+	}
+	return atlas
+}
+
 func main() {
-	var atlas = make(map[string]*city)
-	addDirection(atlas, "Foo", "Bar", "north")
-	addDirection(atlas, "Boo", "Bar", "east")
-	addDirection(atlas, "Foo", "Boo", "west")
+	argv := os.Args
+	if len(argv) != 3 {
+		panic("must provide 2 command line args, numAliens and inputfile")
+	}
+	// numAliens, err := strconv.Atoi(argv[1])
+	// if err != nil {
+	// 	panic(err)
+	// }
+	fileName := strings.TrimSpace(argv[2])
+	atlas := parseFile(fileName)
 	printAtlas(atlas)
 }
