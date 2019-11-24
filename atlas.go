f900133cf805d41a150748d9735bd26101b44de0
Jameson Evans <6jh7ln@gmail.com>
2019-11-24 15:17:59 -0600

    addDirection working

--- atlas.go
+++ atlas.go
@@ -10,9 +10,35 @@ type city struct {
 	w    *city
 }
 
-func newCity(name string, north, south, east, west *city) *city {
-	new := city{name: name, n: north, s: south, e: east, w: west}
+func newCity(name string) *city {
+	new := city{name: name}
 	return &new
+}
+
+//these helpers will keep our atlas in sync but will only be available through SetDirection()
+func setNorth(src, dest *city) {
+	src.n = dest
+	if dest != nil {
+		dest.s = src
+	}
+}
+func setSouth(src, dest *city) {
+	src.s = dest
+	if dest != nil {
+		dest.n = src
+	}
+}
+func setEast(src, dest *city) {
+	src.e = dest
+	if dest != nil {
+		dest.w = src
+	}
+}
+func setWest(src, dest *city) {
+	src.w = dest
+	if dest != nil {
+		dest.e = src
+	}
 }
 
 func printCity(c *city) {
@@ -32,9 +58,33 @@ func printCity(c *city) {
 	fmt.Println()
 }
 
+func addDirection(atlas map[string]*city, src, dest, direction string) {
+	sourceCity, exists := atlas[src]
+	if !exists {
+		atlas[src] = newCity(src)
+		sourceCity = atlas[src]
+	}
+	destCity, exists := atlas[dest]
+	if !exists {
+		atlas[dest] = newCity(dest)
+		destCity = atlas[dest]
+	}
+	if direction == "north" {
+		setNorth(sourceCity, destCity)
+	} else if direction == "south" {
+		setSouth(sourceCity, destCity)
+	} else if direction == "east" {
+		setEast(sourceCity, destCity)
+	} else if direction == "west" {
+		setWest(sourceCity, destCity)
+	}
+
+}
+
 func main() {
 	var atlas = make(map[string]*city)
-	atlas["Foo"] = newCity("Foo", nil, nil, nil, nil)
-	atlas["Bar"] = newCity("Bar", nil, atlas["Foo"], nil, nil)
-	printCity(atlas["Bar"])
+	// atlas["Foo"] = newCity("Foo")
+	// atlas["Bar"] = newCity("Bar")
+	addDirection(atlas, "Foo", "Bar", "north")
+	printCity(atlas["Foo"])
 }
